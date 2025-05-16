53d82e7e343e4e14db91f043e865ad924d85d06b
Hannah A. Patellis <hpatellis@icloud.com>
2025-05-16 15:23:24 -0500

    Initial page config

--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
-<?php
-
-print_r("Hiya");
-
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Minnesota Retro Technology Club</title>
+    <link href="/assets/css/reset.css" rel="stylesheet">
+    <link href="/assets/css/custom.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav>
+      <img id="flag-wordmark" src="/assets/svg/wordmark.svg" alt="Minnesota Retro Technology Club" />
+      <img id="flag-logo" src="/assets/img/mnrtc.png" alt="Minnesota Retro Technology Club logo" />
+    </nav>
+    <main>
+      <h1>Hello, world! Coming soon!</h1>
+    </main>
+  </body>
+</html>
