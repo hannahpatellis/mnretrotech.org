1120e87974106e982f5be2c1e4843e56965b9bbd
Hannah A. Patellis <hpatellis@icloud.com>
2025-05-20 16:00:25 -0500

    Adds 404, 403, and 500 error pages

---
+++ public/500.php
@@ -0,0 +1,37 @@
+<?php $title="Error 500 | "; require_once("partials/header.php"); ?>
+
+    <nav>
+      <img id="flag-wordmark" src="/assets/svg/wordmark.svg" alt="Minnesota Retro Technology Club" />
+      <img id="flag-logo" src="/assets/img/mnrtc.png" alt="Minnesota Retro Technology Club logo" />
+    </nav>
+    <main>
+      <h1>Server error</h1>
+      <p>Error 500</p>
+      
+      <hr />
+
+      <div id="list">
+        <a href="https://forms.gle/JMxNJpo3hgEccBZZ7" title="Interest form on Google Forms" class="entry-wrap">
+          <div class="entry">
+            Fill out the interest form
+          </div>
+        </a>
+        <a href="/proposal.php" title="Club proposal page" class="entry-wrap">
+          <div class="entry">
+            Club proposal
+          </div>
+        </a>
+        <a href="/about.php" title="Who's behind this club page" class="entry-wrap">
+          <div class="entry">
+            Who's behind this club?
+          </div>
+        </a>
+        <a href="https://discord.gg/hF9wh6gPcP" title="Discord invitation link" class="entry-wrap">
+          <div class="entry">
+            <img id="discord-icn" src="/assets/svg/discord.svg" />Join the Discord
+          </div>
+        </a>
+      </div>
+    </main>
+
+<?php require_once("partials/footer.php"); ?>
